f005a7fa30230d653d503c2e57d21d53c67d091e
PayneJoe <yuanpingzhou@sensetime.com>
2017-03-17 10:26:01 +0800

    clean build files

--- build/CMakeFiles/Recommendation.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Recommendation.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/RankingService/IOUtils/CMakeFiles/IOUtils.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libIOUtils.pdb"
-  "libIOUtils.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/IOUtils.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
